de503805f47243ecf9f1cd69a34c76763e668238
tuonux <tuonux0@gmail.com>
2023-09-23 17:37:56 +0200

    v1.0.0

---
+++ README.md
@@ -0,0 +1,49 @@
+# GH File importer
+
+Author: tuonux \<tuonux0@gmail.com\>
+
+With this tool you can import your files from your computer directly into the game.
+
+**NB: Text file only, not images or something or will write the raw big data of them**
+
+## Requirements
+
+- Node.JS (https://nodejs.org/it)
+
+## How to
+
+- Open the game
+- If you have FileExplorer.exe or Terminal.exe openend in the game close both
+- Download this repository
+- Go under the gh-file-importer folder
+- Put or write your files in the "src" folder
+- Launch the "run.bat" executable
+- Enter the right path of your game where the Grey Hack.exe is ( see the section "How to get the game path" )
+- Type enter and wait until the process will be completed.
+
+## License
+
+This program is released under the MIT license.
+
+Copyright (c) 2023 tuonux
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,146 @@
+const sqlite3 = require("sqlite3").verbose();
+const prompt = require("prompt-sync")();
+const { Console } = require("console");
+const fs = require("fs");
+const md5 = require("md5");
+const getPrompt = (str) => {
+  console.log(str);
+  return prompt("> ");
+};
+
+const defaultPath = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Grey Hack";
+
+let dbBuffer = null;
+let dbPath = null;
+console.clear();
+console.log(`GH - Importer tool\nmade with <3 by tuonux`);
+if (fs.existsSync("./config.ini")) {
+  dbPath = fs.readFileSync("./config.ini").toString();
+  console.log("Attempt to get DB buffer...");
+  dbBuffer = fs.readFileSync(fs.readFileSync("./config.ini").toString());
+}
+while (!dbBuffer) {
+  console.log("Get the path of GH installation directory under Steam -> Library -> Right click on Grey Hack -> Properties -> Installed Files -> Browse\n" + "Then copy and paste the path for the explorer address bar here\n");
+  console.log("Default is: C:\\Program Files (x86)\\Steam\\steamapps\\common\\Grey Hack");
+  let gamePath = defaultPath;
+  const iDirectory = getPrompt("Enter the game installation path (leave empty for default)");
+  if (iDirectory != "") {
+    gamePath = iDirectory;
+  }
+  dbPath = (gamePath + "\\Grey Hack_Data\\GreyHackDB.db").split(/\ /).join(" ");
+  console.log("Check that the DB still present...");
+
+  try {
+    console.log("Attempt to get DB buffer...");
+    dbBuffer = fs.readFileSync(dbPath);
+  } catch {
+    dbBuffer = null;
+    dbPath = null;
+    console.log("\n!!! Something goes wrong, repeat the steps !!!\n");
+  }
+}
+console.log(dbBuffer);
+console.log("Grey Hack DB Found!");
+fs.writeFileSync("./config.ini", dbPath.toString());
+console.log("Saved ./config.ini with Grey Hack DB path");
+console.log("Attempt to connect to DB...");
+try {
+  const db = new sqlite3.Database(dbPath);
+  db.serialize(async () => {
+    const doQuery = (query, params = []) => {
+      return new Promise((resolve) => {
+        db.run(query, params, function () {
+          resolve(this);
+        });
+      });
+    };
+    console.log("DB Ready.");
+    console.log("Attempt to read ./src folder...");
+    const srcFolder = fs.readdirSync("./src");
+    const srcFiles = [];
+    for (f of srcFolder) {
+      if (f == "." || f == ".." || f == ".gitkeep") continue;
+      srcFiles.push({
+        ID: md5(Date.now() + Math.floor(Math.random() * 999999999)),
+        name: f,
+        content: fs.readFileSync("./src/" + f).toString(),
+        binary: 0,
+      });
+    }
+    console.log("Reading of ./src folder done.");
+    console.log(`${srcFiles.length} file founds!`);
+    for (f of srcFiles) {
+      const queryStatus = await doQuery(`INSERT INTO Files (ID, Content, refCount) VALUES(?, ?, ?)`, [f.ID, f.content, 1]);
+    }
+    console.log("Files generated succesfully.");
+    console.log("Attempt to update local player computer...");
+    db.each("SELECT FileSystem FROM Computer WHERE IsPlayer = 1", (err, row) => {
+      const rowJson = JSON.parse(row.FileSystem);
+      let pathToReach = "/home";
+      let currentObj = rowJson;
+      console.log("Attempt to read /home on the player computer...");
+      for (path of pathToReach.split("/")) {
+        for (f of currentObj.folders) {
+          if (f.nombre == path) {
+            currentObj = f;
+            break;
+          }
+        }
+      }
+      console.log("/home folder found.");
+      console.log("Attempt to get the user of the player computer...");
+      let username = "";
+      for (f of currentObj.folders) {
+        if (f.nombre == "guest") continue;
+        username = f.nombre;
+        currentObj = f;
+        break;
+      }
+      if (username == "") throw "No username folder founds. Aborted.";
+      console.log("User folder found.");
+      console.log("Player name is: " + username);
+      for (f of srcFiles) {
+        console.log("Attempt to push ./src/" + f.name + " in /home/" + username + " folder object");
+        const o = {
+          ID: f.ID,
+          allowImport: false,
+          comando: "",
+          desc: null,
+          group: username,
+          helperImport: null,
+          isBinario: false,
+          isDefaultContent: false,
+          isEditedOtherPlayer: false,
+          isProtected: false,
+          missionID: "",
+          nombre: f.name,
+          origOwnerID: "",
+          owner: username,
+          permisos: {
+            permisos: "-rwxrwx---",
+          },
+          precio: 0,
+          process: "",
+          saved: true,
+          serverPath: "",
+          size: 0,
+          typeFile: f.binary ? 1 : 0,
+        };
+        console.log("Remove previously object with name " + o.nombre);
+        currentObj.files = currentObj.files.filter((e) => e.nombre != o.nombre);
+        console.log("Object updated succesfully");
+        currentObj.files.push(o);
+      }
+      console.log("/home/" + username + " object updated successfully");
+      console.log("Attempt to update computer player data with new status...");
+      // console.log(JSON.stringify(rowJson, null, 4));
+      fs.writeFileSync("./updated.json", JSON.stringify(rowJson, null, 4));
+      doQuery("UPDATE Computer SET FileSystem = ? WHERE IsPlayer = 1", [JSON.stringify(rowJson)]);
+      console.log("\n\n\nPLAYER COMPUTER DATA UPDATED SUCCESSFULLY. NOW YOU CAN LAUNCH THE GAME :)\n\n\n");
+      prompt("Type enter to quit the program...");
+    });
+  });
+} catch (e) {
+  console.log("!!! Something goes wrong. Aborted.!!!");
+  console.log(e);
+}
